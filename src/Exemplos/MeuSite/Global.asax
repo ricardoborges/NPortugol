ca165f9ddc425abdd2d6c11d750e2564fbeb0bf9
Ricardo Borges <ricardoborges@gmail.com>
2014-02-06 16:58:33 -0300

    example

---
+++ src/Exemplos/MeuSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MeuSite.Global" Language="C#" %>
---
+++ src/Exemplos/MeuSite/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿using MeuSite.Modulos;
+using NPortugol.Web.Core;
+
+namespace MeuSite
+{
+    public class Global : NPWebApp
+    {
+        public override void InstalarModulos()
+        {
+            Container.Install(new Utilidades());
+        }
+    }
+}
